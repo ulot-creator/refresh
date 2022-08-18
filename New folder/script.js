2e2679de96194e4f11ca6e608d295bde20d0e0b1
ulot-creator <ephraimorimolade608@gmail.com>
2022-08-18 10:53:13 +0100

    ,,

---
+++ New folder/script.js
@@ -0,0 +1,5 @@
+const titles = document.querySelectorAll('p');
+const first = document.querySelector('#first');
+const second = document.querySelector('#second');
+const third = document.querySelector('#third');
+const fourth = document.querySelector('#fourth');
